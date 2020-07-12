72ce20bb38404eed56e497f48213a371a9d4ffb9
indian954 <hiteshkardam.hk@gmail.com>
2020-07-13 02:26:22 +0530

    Port VL53l1x ULD API to ESP32, referencing pololu's full fledge API. Also, arduino does not support relative paths?

--- vl53l1_platform.cpp
+++ vl53l1_platform.cpp
@@ -33,43 +33,128 @@
 * 
 */
 
+#include <Arduino.h>
+#include <Wire.h>
+
 #include "vl53l1_platform.h"
+#include "vl53l1X_api.h"
 #include <string.h>
 #include <time.h>
 #include <math.h>
 
 int8_t VL53L1_WriteMulti( uint16_t dev, uint16_t index, uint8_t *pdata, uint32_t count) {
-	return 0; // to be implemented
+  while (count > 0)
+  {
+    Wire.beginTransmission(dev >> 1);
+    Wire.write((index >> 8) & 0xff);
+    Wire.write(index & 0xff);
+
+    uint8_t writing = 0;
+
+    while (count > 0 && Wire.write(*pdata) != 0)
+    {
+      pdata++;
+      writing++;
+      count--;
+    }
+
+    if (writing == 0 || Wire.endTransmission() != 0) { return 1; }
+    index += writing;
+  }
+	return 0;
 }
 
 int8_t VL53L1_ReadMulti(uint16_t dev, uint16_t index, uint8_t *pdata, uint32_t count){
-	return 0; // to be implemented
+	Wire.beginTransmission(dev >> 1);
+	Wire.write((index >> 8) & 0xff);
+	Wire.write(index & 0xff);
+	if (Wire.endTransmission() != 0) { return 1; }
+	
+	while (count > 0)
+	{
+		uint8_t reading = Wire.requestFrom(dev >> 1, count);
+		if (reading == 0) { return 1; }
+		count -= reading;
+		
+		while (reading-- > 0)
+		{
+			*pdata++ = Wire.read();
+		}
+	}
+	
+	return 0;
 }
 
 int8_t VL53L1_WrByte(uint16_t dev, uint16_t index, uint8_t data) {
-	return 0; // to be implemented
+	Wire.beginTransmission(dev >> 1);
+	Wire.write((index >> 8) & 0xff);
+	Wire.write(index & 0xff);
+	Wire.write(data);
+	
+	return (Wire.endTransmission() == 0 ? 0 : 1);
 }
 
 int8_t VL53L1_WrWord(uint16_t dev, uint16_t index, uint16_t data) {
-	return 0; // to be implemented
+	Wire.beginTransmission(dev >> 1);
+	Wire.write((index >> 8) & 0xff);
+	Wire.write(index & 0xff);
+	Wire.write((data >> 8) & 0xff);
+	Wire.write(data & 0xff);
+	
+	return (Wire.endTransmission() == 0 ? 0 : 1);
 }
 
 int8_t VL53L1_WrDWord(uint16_t dev, uint16_t index, uint32_t data) {
-	return 0; // to be implemented
+	Wire.beginTransmission(dev >> 1);
+	Wire.write((index >> 8) & 0xff);
+	Wire.write(index & 0xff);
+	Wire.write((data >> 24) & 0xff);
+	Wire.write((data >> 16) & 0xff);
+	Wire.write((data >> 8) & 0xff);
+	Wire.write(data & 0xff);
+	
+	return (Wire.endTransmission() == 0 ? 0 : 1);
 }
 
 int8_t VL53L1_RdByte(uint16_t dev, uint16_t index, uint8_t *data) {
-	return 0; // to be implemented
+	Wire.beginTransmission(dev >> 1);
+	Wire.write((index >> 8) & 0xff);
+	Wire.write(index & 0xff);
+	if (Wire.endTransmission() != 0) { return 1; }
+	if (Wire.requestFrom(dev >> 1, 1) != 1) { return 1; }
+	*data = Wire.read();
+
+	return 0;
 }
 
 int8_t VL53L1_RdWord(uint16_t dev, uint16_t index, uint16_t *data) {
-	return 0; // to be implemented
+	Wire.beginTransmission(dev >> 1);
+	Wire.write((index >> 8) & 0xff);
+	Wire.write(index & 0xff);
+	if (Wire.endTransmission() != 0) { return 1; }
+	if (Wire.requestFrom(dev >> 1, 2) != 2) { return 1; }
+	*data = (uint16_t)Wire.read() << 8;
+	*data |= Wire.read();
+	
+	return 0;
 }
 
 int8_t VL53L1_RdDWord(uint16_t dev, uint16_t index, uint32_t *data) {
-	return 0; // to be implemented
+	Wire.beginTransmission(dev >> 1);
+	Wire.write((index >> 8) & 0xff);
+	Wire.write(index & 0xff);
+	if (Wire.endTransmission() != 0) { return 1; }
+	if (Wire.requestFrom(dev >> 1, 4) != 4) { return 1; }
+	*data = (uint32_t)Wire.read() << 24;
+	*data |= (uint32_t)Wire.read() << 16;
+	*data |= (uint16_t)Wire.read() << 8;
+	*data |= Wire.read();
+	
+	return 0;
 }
 
 int8_t VL53L1_WaitMs(uint16_t dev, int32_t wait_ms){
-	return 0; // to be implemented
+	delay(wait_ms);
+	
+	return 0;
 }

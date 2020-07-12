446c5253ba36070f91cabae2efd6e197e71aa00a
indian954 <hiteshkardam.hk@gmail.com>
2020-07-12 21:48:13 +0530

    Switch to ST VL53L1X ULD API

--- covid6feet.ino
+++ covid6feet.ino
@@ -1,6 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include "./api/vl53l1_platform.h"
 
+VL53L1_Dev_t dev;        
+VL53L1_DEV Dev = &dev;
+
+void setup()
+{
+  uint8_t byteData;
+  uint16_t wordData;
+
+  Wire.begin();
+  Wire.setClock(400000);
+  Serial.begin(115200);
+
+  Dev->I2cDevAddr = 0x52; //I2C address of the sensor
+
+  //VL53L1_WaitDeviceBooted(Dev);
+  //VL53L1_software_reset(Dev);
+
+  VL53L1_RdByte(Dev, 0x010F, &byteData);
+  Serial.print(F("VL53L1X Model_ID: "));
+  Serial.println(byteData, HEX);
+  VL53L1_RdByte(Dev, 0x0110, &byteData);
+  Serial.print(F("VL53L1X Module_Type: "));
+  Serial.println(byteData, HEX);
+  VL53L1_RdWord(Dev, 0x010F, &wordData);
+  Serial.print(F("VL53L1X: "));
+  Serial.println(wordData, HEX);
 }
 
 void loop() {
